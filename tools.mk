57a397cfa99352a1e48217dcc700e91aa510981b
Ryan Hartlage <ryanplusplus@gmail.com>
2021-02-27 14:34:05 -0500

    Extracted dfp.mk

--- tools.mk
+++ tools.mk
@@ -8,17 +8,7 @@ include $(__avr_gcc_tools_path)gcc/tools.mk
 
 include $(__avr_gcc_tools_path)upload.mk
 include $(__avr_gcc_tools_path)debug.mk
-
-.PHONY: dfp
-dfp: $(__avr_gcc_tools_path)dfp/$(DEVICE_PACK)/package.content
-
-$(__avr_gcc_tools_path)dfp/$(DEVICE_PACK)/package.content: $(__avr_gcc_tools_path)dfp/download/$(DEVICE_PACK).atpack
-	@unzip $(__avr_gcc_tools_path)dfp/download/$(DEVICE_PACK).atpack -d $(__avr_gcc_tools_path)dfp/$(DEVICE_PACK)
-	@touch $@
-
-$(__avr_gcc_tools_path)dfp/download/$(DEVICE_PACK).atpack:
-	@mkdir -p $(dir $@)
-	@cd $(dir $@) && wget http://packs.download.atmel.com/$(DEVICE_PACK).atpack
+include $(__avr_gcc_tools_path)dfp.mk
 
 .PHONY: install_toolchain
 install_toolchain:
