8c4ee92f68684ccfba62784fb6dfb238c02fb5a4
Ryan Hartlage <ryanplusplus@gmail.com>
2021-02-27 14:24:49 -0500

    Added install_toolchain and moved dfp into tools directory

--- tools.mk
+++ tools.mk
@@ -1,7 +1,7 @@
 __avr_gcc_tools_path := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 
 BUILD_DEPS += dfp
-SYS_INC_DIRS += dfp/$(DEVICE_PACK)/include
+SYS_INC_DIRS += $(__avr_gcc_tools_path)dfp/$(DEVICE_PACK)/include
 
 TOOLCHAIN_PREFIX := avr-
 include $(__avr_gcc_tools_path)gcc/tools.mk
@@ -10,12 +10,16 @@ include $(__avr_gcc_tools_path)upload.mk
 include $(__avr_gcc_tools_path)debug.mk
 
 .PHONY: dfp
-dfp: dfp/$(DEVICE_PACK)/package.content
+dfp: $(__avr_gcc_tools_path)dfp/$(DEVICE_PACK)/package.content
 
-dfp/$(DEVICE_PACK)/package.content: dfp/download/$(DEVICE_PACK).atpack
-	@unzip dfp/download/$(DEVICE_PACK).atpack -d dfp/$(DEVICE_PACK)
+$(__avr_gcc_tools_path)dfp/$(DEVICE_PACK)/package.content: $(__avr_gcc_tools_path)dfp/download/$(DEVICE_PACK).atpack
+	@unzip $(__avr_gcc_tools_path)dfp/download/$(DEVICE_PACK).atpack -d $(__avr_gcc_tools_path)dfp/$(DEVICE_PACK)
 	@touch $@
 
-dfp/download/$(DEVICE_PACK).atpack:
+$(__avr_gcc_tools_path)dfp/download/$(DEVICE_PACK).atpack:
 	@mkdir -p $(dir $@)
 	@cd $(dir $@) && wget http://packs.download.atmel.com/$(DEVICE_PACK).atpack
+
+.PHONY: install_toolchain
+install_toolchain:
+	sudo apt-get install gcc-avr binutils-avr gdb-avr avr-libc avrdude avarice
