94ec919300e4a38df8456ed258e0d35cc401edb0
Ryan Hartlage <ryanplusplus@gmail.com>
2021-02-28 13:32:19 -0500

    avarice with atmel ice support

--- debug.mk
+++ debug.mk
@@ -1,11 +1,18 @@
 __avr_gcc_tools_debug_path := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 __dwdebug_path := $(__avr_gcc_tools_debug_path)bin/dwdebug-$(shell uname)
 
-.PHONY: $(BUILD_DIR)/debug-server-avarice
-$(BUILD_DIR)/debug-server-avarice:
+.PHONY: $(BUILD_DIR)/debug-server-avarice-dragon
+$(BUILD_DIR)/debug-server-avarice-dragon:
 	@echo "#!/bin/bash" > $@
 	@echo "PORT=\`echo \"'\$$*'\" | sed 's/.*gdb_port \([^ ]*\).*/\\\1/'\`" >> $@
-	@echo "avarice -g -w :\$$PORT" >> $@
+	@echo "avarice -P $(MCU) -g -w :\$$PORT" >> $@
+	@chmod +x $@
+
+.PHONY: $(BUILD_DIR)/debug-server-avarice-ice
+$(BUILD_DIR)/debug-server-avarice-ice:
+	@echo "#!/bin/bash" > $@
+	@echo "PORT=\`echo \"'\$$*'\" | sed 's/.*gdb_port \([^ ]*\).*/\\\1/'\`" >> $@
+	@echo "avarice -P $(MCU) --edbg -w :\$$PORT" >> $@
 	@chmod +x $@
 
 .PHONY: $(BUILD_DIR)/debug-server-dwdebug
@@ -15,4 +22,4 @@ $(BUILD_DIR)/debug-server-dwdebug:
 	@chmod +x $@
 
 .PHONY: debug-deps
-debug-deps: $(BUILD_DIR)/debug-server-avarice $(BUILD_DIR)/debug-server-dwdebug $(BUILD_DIR)/$(TARGET).hex
+debug-deps: $(BUILD_DIR)/debug-server-avarice-dragon $(BUILD_DIR)/debug-server-avarice-ice $(BUILD_DIR)/debug-server-dwdebug $(BUILD_DIR)/$(TARGET).hex
--- upload.mk
+++ upload.mk
@@ -19,11 +19,11 @@ endif
 ifeq ($(UPLOAD_TYPE),avrdude)
 .PHONY: fuse
 fuse:
-	avrdude -c $(AVRDUDE_PROGRAMMER_TYPE) -p $(MCU) $(AVRDUDE_PROGRAMMER_ARGS) $(FUSE_ARGS)
+	@avrdude -c $(AVRDUDE_PROGRAMMER_TYPE) -p $(MCU) $(AVRDUDE_PROGRAMMER_ARGS) $(FUSE_ARGS)
 
 .PHONY: upload
 upload: $(BUILD_DIR)/$(TARGET).hex
-	avrdude -c $(AVRDUDE_PROGRAMMER_TYPE) -p $(MCU) $(AVRDUDE_PROGRAMMER_ARGS) -U flash:w:$<
+	@avrdude -c $(AVRDUDE_PROGRAMMER_TYPE) -p $(MCU) $(AVRDUDE_PROGRAMMER_ARGS) -U flash:w:$<
 
 .PHONY: erase
 erase:
