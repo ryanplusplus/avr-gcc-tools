d22415bf7be5349535b140b824a586a76627f845
Ryan Hartlage <ryanplusplus@gmail.com>
2021-07-05 14:44:45 -0400

    Point to microchip.com for DFPs

--- dfp.mk
+++ dfp.mk
@@ -7,4 +7,4 @@ $(__avr_gcc_tools_path)dfp/$(DEVICE_PACK)/package.content: $(__avr_gcc_tools_pat
 
 $(__avr_gcc_tools_path)dfp/download/$(DEVICE_PACK).atpack:
 	@mkdir -p $(dir $@)
-	@cd $(dir $@) && wget http://packs.download.atmel.com/$(DEVICE_PACK).atpack
+	@cd $(dir $@) && wget http://packs.download.microchip.com/$(DEVICE_PACK).atpack
