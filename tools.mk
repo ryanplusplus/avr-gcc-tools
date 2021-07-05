b8b25589b8619645974197a02a2b4493ecc54f97
Ryan Hartlage <ryanplusplus@gmail.com>
2021-07-05 08:56:10 -0400

    Added docs.mk

--- tools.mk
+++ tools.mk
@@ -8,6 +8,7 @@ include $(__avr_gcc_tools_path)gcc/tools.mk
 include $(__avr_gcc_tools_path)upload.mk
 include $(__avr_gcc_tools_path)debug.mk
 include $(__avr_gcc_tools_path)dfp.mk
+include $(__avr_gcc_tools_path)docs.mk
 
 .PHONY: install_toolchain
 install_toolchain:
