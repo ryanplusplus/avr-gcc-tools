1c216136428358e0b507504390fdd00884657d56
Ryan Hartlage <ryanplusplus@gmail.com>
2021-12-30 20:19:15 -0500

    Updated docs

--- docs.mk
+++ docs.mk
@@ -3,14 +3,29 @@ HARDWARE_MANUAL_URL := http://ww1.microchip.com/downloads/en/DeviceDoc/ATmega48A
 DATASHEET_URL := http://ww1.microchip.com/downloads/en/DeviceDoc/ATmega48A-PA-88A-PA-168A-PA-328-P-DS-DS40002061A.pdf
 endif
 
+ifeq ($(MCU),atmega4809)
+HARDWARE_MANUAL_URL := https://ww1.microchip.com/downloads/en/DeviceDoc/ATmega4808-09-DataSheet-DS40002173B.pdf
+DATASHEET_URL := https://ww1.microchip.com/downloads/en/DeviceDoc/ATmega4808-09-DataSheet-DS40002173B.pdf
+endif
+
+ifeq ($(MCU),attiny167)
+HARDWARE_MANUAL_URL := http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-8265-8-bit-AVR-Microcontroller-tinyAVR-ATtiny87-ATtiny167_datasheet.pdf
+DATASHEET_URL := http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-8265-8-bit-AVR-Microcontroller-tinyAVR-ATtiny87-ATtiny167_datasheet.pdf
+endif
+
+ifeq ($(MCU),attiny416)
+HARDWARE_MANUAL_URL := https://ww1.microchip.com/downloads/en/DeviceDoc/ATtiny212-214-412-414-416-DataSheet-DS40002287A.pdf
+DATASHEET_URL := https://ww1.microchip.com/downloads/en/DeviceDoc/ATtiny212-214-412-414-416-DataSheet-DS40002287A.pdf
+endif
+
 ifeq ($(MCU),attiny85)
 HARDWARE_MANUAL_URL := http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-2586-AVR-8-bit-Microcontroller-ATtiny25-ATtiny45-ATtiny85_Datasheet.pdf
 DATASHEET_URL := http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-2586-AVR-8-bit-Microcontroller-ATtiny25-ATtiny45-ATtiny85_Datasheet.pdf
 endif
 
-ifeq ($(MCU),atmega4809)
-HARDWARE_MANUAL_URL := https://ww1.microchip.com/downloads/en/DeviceDoc/ATmega4808-09-DataSheet-DS40002173B.pdf
-DATASHEET_URL := https://ww1.microchip.com/downloads/en/DeviceDoc/ATmega4808-09-DataSheet-DS40002173B.pdf
+ifeq ($(MCU),attiny88)
+HARDWARE_MANUAL_URL := https://ww1.microchip.com/downloads/en/DeviceDoc/doc8008.pdf
+DATASHEET_URL := https://ww1.microchip.com/downloads/en/DeviceDoc/doc8008.pdf
 endif
 
 ifneq ($(HARDWARE_MANUAL_URL),)
